9381bbe3be96d8f14a57eaa61dbee6e5a4e76efa
John Shoemaker <shoemjoh@gmail.com>
2024-10-23 23:03:29 -0400

    added in property methods to validate inputs

--- lib/models/book.py
+++ lib/models/book.py
@@ -7,15 +7,39 @@ class Book:
         self.author = author
         self.genre_id = genre_id
 
+    @property
+    def name(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if not title or len(title) < 2:
+            raise ValueError("Book title must be at least 2 characters long.")
+        self._title = title
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        if not author or len(author) < 2:
+            raise ValueError("Author name must be at least 2 characters long.")
+        self._author = author
+
     #add a book to the table books
     @classmethod
     def add_book (cls, title, author, genre_id):
         sql = '''
         INSERT INTO books (title, author, genre_id) VALUES (?, ?, ?)
         '''
-        CURSOR.execute(sql, (title, author, genre_id))
-        CONN.commit()
-        print(f"Book '{title}' by {author} added to genre ID {genre_id}")
+        try:
+            book = cls(title, author, genre_id)
+            CURSOR.execute(sql, (title, author, genre_id))
+            CONN.commit()
+            print(f"Book '{title}' by {author} added to genre ID {genre_id}")
+        except sqlite3.IntegrityError as e:
+            print(f"Error: {str(e)}")
 
     # get all the books of a certain genre
     @classmethod
