b97eecded0e23f9d918c334839ee5ebcdb765e12
John Shoemaker <shoemjoh@gmail.com>
2024-10-24 19:54:07 -0400

    updated genre class

--- lib/models/genre.py
+++ lib/models/genre.py
@@ -1,60 +1,77 @@
-import sqlite3
-from models import CONN, CURSOR
+from . import CONN, CURSOR
 
 class Genre:
+    all = {}
 
-    _genres = {}
-
-    def __init__(self, name):
+    def __init__(self, name, id=None):
+        self.id = id
         self.name = name
-        self.id = None
-        Genre._genres[self.name] = self
 
-    #add new genre to database
+    def __repr__(self):
+        return f"<Genre {self.id}: {self.name}>"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
+
+    def save(self):
+        if self.id is None:
+            sql = "INSERT INTO genres (name) VALUES (?)"
+            CURSOR.execute(sql, (self.name,))
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+        else:
+            sql = "UPDATE genres SET name = ? WHERE id = ?"
+            CURSOR.execute(sql, (self.name, self.id))
+            CONN.commit()
+
+        type(self).all[self.id] = self
+
     @classmethod
     def add_genre(cls, name):
-        if name in cls._genres:
-            print(f"Error: Genre '{name}' already exists.")
-            return
         genre = cls(name)
-        sql = """
-            INSERT INTO genres (name) VALUES (?)
-        """
-        try:
-            CURSOR.execute(sql, (name,))
-            CONN.commit()
-            genre.id = CURSOR.lastrowid
-            print(f"Genre '{name}' added successfully!")
-        except sqlite3.IntegreityError:
-            print(f"Error: Genre '{name}' already exists in database.")
-    
-    #get all genres
-    @classmethod
-    def get_all_genres(cls):
-        sql = """
-        SELECT * FROM genres
-        """
-        CURSOR.execute(sql)
-        genres = CURSOR.fetchall()
-        return genres
-    
-    #delete a genre
-    @classmethod
-    def delete_genre(cls, genre_id):
-        sql = """
-            DELETE FROM genres WHERE id = ?
-        """
-        CURSOR.execute(sql, (genre_id,))
-        CONN.commit()
-        cls._genres = [g for g in cls._genres if g.id != genre_id]
-        print(f"GENRE ID {genre_id} deleted.")
+        genre.save()
+        return genre
 
-    #find a genre by name
+    @classmethod
+    def instance_from_db(cls, row):
+        genre = cls.all.get(row[0])
+        if genre:
+            genre.name = row[1]
+        else:
+            genre = cls(row[1], id=row[0])
+            cls.all[genre.id] = genre
+        return genre
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = "SELECT * FROM genres WHERE id = ?"
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
     @classmethod
     def find_by_name(cls, name):
-        sql = """
-        SELECT * FROM genres WHERE name = ?
-        """
-        CURSOR.execute(sql, (name,))
-        genre = CURSOR.fetchone()
-        return genre
+        sql = "SELECT * FROM genres WHERE name = ?"
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    def delete(self):
+        sql = "DELETE FROM genres WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+
+        del type(self).all[self.id]
+        self.id = None
+
+    @classmethod
+    def get_all(cls):
+        sql = "SELECT * FROM genres"
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
