be4c2dbf34020fff0e1b28f999e38e721064b4fc
John Shoemaker <shoemjoh@gmail.com>
2024-10-23 20:29:18 -0400

    initializing the database with the genre table

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,16 @@
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
+#establish connection and create cursor
+CONN = sqlite3.connect('library.db')
 CURSOR = CONN.cursor()
+
+#initialize the database
+def initialize_db():
+    CURSOR.execute('''
+        CREATE TABLE IF NOT EXISTS genres (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE
+        )
+    ''')
+
+    CONN.commit()
