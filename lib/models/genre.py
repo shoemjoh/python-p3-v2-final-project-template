cc3a832651a201daa26a5f65ef99f219b52e4262
John Shoemaker <shoemjoh@gmail.com>
2024-10-23 21:11:26 -0400

    added class methods delete genre and find genre by name, returning it

--- lib/models/genre.py
+++ lib/models/genre.py
@@ -19,6 +19,7 @@ class Genre:
             print(f"Error: Genre '{name}' already exists.")
     
     #get all genres
+    @classmethod
     def get_all_genres(cls):
         sql = """
         SELECT * FROM genres
@@ -26,4 +27,23 @@ class Genre:
         CURSOR.execute(sql)
         genres = CURSOR.fetchall()
         return genres
-        
+    
+    #delete a genre
+    @classmethod
+    def delete_genre(cls, genre_id):
+        sql = """
+            DELETE FROM genres WHERE id = ?
+        """
+        CURSOR.execute(sql, (genre_id,))
+        CONN.commit()
+        print(f"GENRE ID {genre_id} deleted.")
+
+    #find a genre by name
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+        SELECT * FROM genres WHERE name = ?
+        """
+        CURSOR.execute(sql, (name,))
+        genre = CURSOR.fetchone()
+        return genre
